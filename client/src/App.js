16e5ab570132dd3ed8bec28f89c47ab3408f311c
plast <plastic1230p@gmail.com>
2023-02-19 14:51:58 +0900

    App.css 수정, 메인 html 설계

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
+import { RightSidebar } from './components/RightSidebar/RightSidebar';
+import { Sidebar } from './components/Sidebar/Sidebar';
 import { MainPage } from './pages/MainPage';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
+      <Sidebar></Sidebar>
       <MainPage></MainPage>
+      <RightSidebar></RightSidebar>
+      <Footer></Footer>
     </div>
   );
 }
--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,9 +1,47 @@
-import { Header } from '../components/Header/Header';
-
 export const MainPage = () => {
   return (
-    <div>
-      <Header></Header>
-    </div>
+    <>
+      <div id="mainbar">
+        <div>
+          <h1>All Questions</h1>
+          <div>
+            <button>Ask Question</button>
+          </div>
+        </div>
+        <div>
+          <div>23,507,409 questions</div>
+          <div>
+            <div>tab</div>
+            <div>
+              <button>Filter</button>
+            </div>
+          </div>
+        </div>
+        <div>
+          {/* questions */}
+          {/* 여기에 map 사용 */}
+          <div>
+            <div>
+              <div>0 votes</div>
+              <div>0 answers</div>
+              <div>2 views</div>
+            </div>
+            <div>
+              <h2>question title</h2>
+              <div>question content</div>
+              <div>
+                <div>tag</div>
+                <div>user</div>
+                <div>time</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>pagination-left</div>
+          <div>pagination-right</div>
+        </div>
+      </div>
+    </>
   );
 };
