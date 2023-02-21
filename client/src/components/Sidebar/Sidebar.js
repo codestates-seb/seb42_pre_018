f0bbe1022a65b294a6d58a546c2ecf5c9755240f
ASOpaper <hiji0907@gmail.com>
2023-02-20 16:10:03 -0800

    사이드바 진행사항 저장

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,3 +1,28 @@
+import styles from './sidebar.module.css';
+
 export const Sidebar = () => {
-  return <div></div>;
+  return (
+    <nav className={styles.sidebar__Nav}>
+      <ol>
+        <li className={styles.sidebar__li}>
+          <a href="a">Home</a>
+        </li>
+      </ol>
+      <h1>PUBLIC</h1>
+      <ol>
+        <li className={styles.sidebar__li}>
+          <a href="a">Questions</a>
+        </li>
+        <li className={styles.sidebar__li}>
+          <a href="https://stackoverflow.com/tags">Tags</a>
+        </li>
+        <li className={styles.sidebar__li}>
+          <a href="a">Users</a>
+        </li>
+        <li className={styles.sidebar__li}>
+          <a href="https://stackoverflow.com/jobs/companies">Companies</a>
+        </li>
+      </ol>
+    </nav>
+  );
 };
