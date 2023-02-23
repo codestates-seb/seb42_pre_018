8d67978d3f6ccd7a7e432a3f2fc13791521124b3
plast <plastic1230p@gmail.com>
2023-02-23 21:04:43 +0900

    footer 위치 변경

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import { Footer } from './components/Footer/Footer';
 import { AuthHeader } from './components/Header/AuthHeader';
 import { AskPage } from './pages/AskPage';
 import { Routes, Route } from 'react-router-dom';
 import { DetailPage } from './pages/DetailPage';
-import SidebarWrapper from './components/SidebarWrapper/SidebarWrapper';
+import LayoutWrapper from './components/LayoutWrapper/LayoutWrapper';
 import { MainPage } from './pages/MainPage';
 
 function App() {
@@ -16,24 +15,23 @@ function App() {
         <Route
           path="/"
           element={
-            <SidebarWrapper>
+            <LayoutWrapper>
               <MainPage />
-            </SidebarWrapper>
+            </LayoutWrapper>
           }
         ></Route>
         {/* 질문 상세 페이지 */}
         <Route
           path="/detail"
           element={
-            <SidebarWrapper>
+            <LayoutWrapper>
               <DetailPage />
-            </SidebarWrapper>
+            </LayoutWrapper>
           }
         ></Route>
         {/* 질문 작성 페이지 */}
         <Route path="/ask" element={<AskPage />}></Route>
       </Routes>
-      <Footer></Footer>
     </div>
   );
 }
--- client/src/components/LayoutWrapper/LayoutWrapper.js
+++ client/src/components/LayoutWrapper/LayoutWrapper.js
@@ -2,8 +2,9 @@
 // children props를 자꾸 에러먹여서 그냥 여기에만 비활성화시켜뒀습니다..
 import { Sidebar } from '../Sidebar/Sidebar';
 import { RightSidebar } from '../RightSidebar/RightSidebar';
+import { Footer } from '../Footer/Footer';
 
-const SidebarWrapper = ({ children }) => {
+const LayoutWrapper = ({ children }) => {
   return (
     <>
       <div className="main__flex">
@@ -11,8 +12,9 @@ const SidebarWrapper = ({ children }) => {
         {children}
         <RightSidebar></RightSidebar>
       </div>
+      <Footer></Footer>
     </>
   );
 };
 
-export default SidebarWrapper;
+export default LayoutWrapper;
