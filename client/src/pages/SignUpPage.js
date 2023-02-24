5bee89c34c009cfea5e86bdd1004775e28b4d49d
plast <plastic1230p@gmail.com>
2023-02-24 10:50:09 +0900

    회원가입 50%완성

--- client/src/pages/SignUpPage.js
+++ client/src/pages/SignUpPage.js
@@ -9,7 +9,7 @@ function SignupPage() {
     <div className={cx('container', 'd-flex')}>
       <div className={cx('d-flex', 'center')}>
         <div className={cx('d-flex', 'center--main')}>
-          <div>
+          <div className={cx('text-wrapper')}>
             <h1>Join the Stack Overflow community</h1>
             <div className={cx('d-flex', 'content')}>
               <div className={cx('d-flex', 'icon')}>
@@ -55,6 +55,10 @@ function SignupPage() {
               </div>
               <div>Earn reputation and badges</div>
             </div>
+            <div className={cx('p-13')}>
+            Collaborate and share knowledge with a private group for FREE.<br></br>
+Get Stack Overflow for Teams free for up to 50 users.
+            </div>
           </div>
           <div className={cx('d-flex', 'form--wrapper')}>
             <div className={cx('buttons', 'd-flex')}>
@@ -86,6 +90,19 @@ function SignupPage() {
                     <input type="password" name='password' />
                     <svg aria-hidden="true" className="s-input-icon js-alert-icon d-none svg-icon iconAlertCircle" width="18" height="18" viewBox="0 0 18 18"><path d="M9 17c-4.36 0-8-3.64-8-8 0-4.36 3.64-8 8-8 4.36 0 8 3.64 8 8 0 4.36-3.64 8-8 8ZM8 4v6h2V4H8Zm0 8v2h2v-2H8Z"></path></svg>
                     <p>비번 에러 메시지</p>
+                  </div>
+                </div>
+                <div className={cx('d-flex')}>
+                  <div>
+                    <input type="checkbox" name='EmailOptIn' id='opt-in' />
+                  </div>
+                  <div>
+                    <label htmlFor="opt-in" className={cx('check-label')}>
+                      Opt-in to receive occasional product updates, user research invitations, company announcements, and digests.
+                    </label>
+                  </div>
+                  <div>
+                    <svg aria-hidden="true" className="svg-icon iconHelpSm" width="14" height="14" viewBox="0 0 14 14"><path d="M7 1C3.74 1 1 3.77 1 7c0 3.26 2.77 6 6 6 3.27 0 6-2.73 6-6s-2.73-6-6-6Zm1.06 9.06c-.02.63-.48 1.02-1.1 1-.57-.02-1.03-.43-1.01-1.06.02-.63.5-1.04 1.08-1.02.6.02 1.05.45 1.03 1.08Zm.73-3.07-.47.3c-.2.15-.36.36-.44.6a3.6 3.6 0 0 0-.08.65c0 .04-.03.14-.16.14h-1.4c-.14 0-.16-.09-.16-.13-.01-.5.11-.99.36-1.42A4.6 4.6 0 0 1 7.7 6.07c.15-.1.21-.21.3-.33.18-.2.28-.47.28-.74.01-.67-.53-1.14-1.18-1.14-.9 0-1.18.7-1.18 1.46H4.2c0-1.17.31-1.92.98-2.36a3.5 3.5 0 0 1 1.83-.44c.88 0 1.58.16 2.2.62.58.42.88 1.02.88 1.82 0 .5-.17.9-.43 1.24-.15.2-.44.47-.86.79h-.01Z"></path></svg>
                   </div>
                 </div>
               </form>
